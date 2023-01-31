2e7d37029797585567f13f3f5c80b2d1f4c3dafe
Shrey Parmar <107606723+ShreyParmar928@users.noreply.github.com>
2023-01-31 08:43:22 +0530

    Create BalancedBinaryTree.cpp

---
+++ Assignment/Assignment 10/BalancedBinaryTree.cpp
@@ -0,0 +1,24 @@
+
+
+class Solution {
+  public:
+    int height(TreeNode* node){
+    if (node == NULL)
+        return 0;
+ 
+    return 1 + max(height(node->left), height(node->right));
+}
+ 
+bool isBalanced(TreeNode* root){
+    if (root == NULL)
+        return 1;
+
+    int lh = height(root->left);
+    int rh = height(root->right);
+ 
+    if (abs(lh - rh) <= 1 && isBalanced(root->left) && isBalanced(root->right))
+        return 1;
+ 
+    return 0;
+}
+};
