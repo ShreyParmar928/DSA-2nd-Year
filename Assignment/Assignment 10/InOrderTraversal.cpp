2a47198a8d5b956d0cc1ddbcb99b4e8d581a5d4d
Shrey Parmar <107606723+ShreyParmar928@users.noreply.github.com>
2023-01-31 08:53:22 +0530

    Create InOrderTraversal.cpp

---
+++ Assignment/Assignment 10/InOrderTraversal.cpp
@@ -0,0 +1,13 @@
+
+class Solution {
+public:
+    vector<int> v;
+    vector<int> inorderTraversal(TreeNode* root) {
+                if(root){
+                  inorderTraversal(root->left);
+                  v.push_back(root->val);
+                  inorderTraversal(root->right);
+                }
+            return v;
+    }
+};
